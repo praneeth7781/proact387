57d033cc3b1db8b14c7e669cd0d251903ddd61e3
praneeth <praneeth.cse.iitb@gmail.com>
2023-04-28 14:04:32 +0530

    CSS changes

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,5 +1,5 @@
 // import React from 'react';
-import { Link ,useMatch,useResolvedPath} from 'react-router-dom';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 import React, { useEffect, useRef, useState } from "react";
 import ReactModal from 'react-modal';
 import Axios from "axios";
@@ -11,121 +11,121 @@ function NavBar() {
 
   Axios.defaults.withCredentials = true;
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    var [fetched, setFetched] = useState(false);
-    var [edited, setEdited] = useState(false);
-    var [coursespresent, setCoursesPresent] = useState(false);
-    var [classestoday, setClassesToday] = useState(false);
-    var instname = useRef("Name");
-    var instid = useRef("212");
-    var instdept = useRef("Comp. Sci.");
-    var coursedata = useRef(null);
-    var [editname, setEditName] = useState(instname.current);
-    var [editdept, setEditDept] = useState(instdept.current);
-    var today = useRef(null);
-    var [editpopup, setEditPopup] = useState(false);
-    // const [instname, setInstName] = useRef("Name");
-    // const [instid, setInstID] = useRef("2000");
-    // const [instdept, setInstDept] = useRef("Comp. Sci.");
-    // const [courses, ]
+  var [fetched, setFetched] = useState(false);
+  var [edited, setEdited] = useState(false);
+  var [coursespresent, setCoursesPresent] = useState(false);
+  var [classestoday, setClassesToday] = useState(false);
+  var instname = useRef("Name");
+  var instid = useRef("212");
+  var instdept = useRef("Comp. Sci.");
+  var coursedata = useRef(null);
+  var [editname, setEditName] = useState(instname.current);
+  var [editdept, setEditDept] = useState(instdept.current);
+  var today = useRef(null);
+  var [editpopup, setEditPopup] = useState(false);
+  // const [instname, setInstName] = useRef("Name");
+  // const [instid, setInstID] = useRef("2000");
+  // const [instdept, setInstDept] = useRef("Comp. Sci.");
+  // const [courses, ]
 
-    var infofetch = async () => {
-        var result = await Axios.get("/instructorinfo").then((response) => {
-            return response;
-        });
-        if (result.data.message) {
-            alert("There seems to be a problem with our server. Please hang on while we try to fix it!");
-            return 0;
-        } else if (result.data.insterror) {
-            alert("Your data is not found in our database");
-            return 0;
-        } else if (result.data.nocourses) {
-            instname.current = result.data.instname;
-            instid.current = result.data.instid;
-            instdept.current = result.data.instdept;
-            setEditDept(instdept.current);
-            setEditName(instname.current);
-            setFetched(true);
-            return 1;
-        } else if (result.data.noclassestoday) {
-            instname.current = result.data.instname;
-            instid.current = result.data.instid;
-            instdept.current = result.data.instdept;
-            coursedata.current = result.data.instinfo.rows;
-            setEditDept(instdept.current);
-            setEditName(instname.current);
-            setCoursesPresent(true);
-            setFetched(true);
-            return 1;
+  var infofetch = async () => {
+    var result = await Axios.get("/instructorinfo").then((response) => {
+      return response;
+    });
+    if (result.data.message) {
+      alert("There seems to be a problem with our server. Please hang on while we try to fix it!");
+      return 0;
+    } else if (result.data.insterror) {
+      alert("Your data is not found in our database");
+      return 0;
+    } else if (result.data.nocourses) {
+      instname.current = result.data.instname;
+      instid.current = result.data.instid;
+      instdept.current = result.data.instdept;
+      setEditDept(instdept.current);
+      setEditName(instname.current);
+      setFetched(true);
+      return 1;
+    } else if (result.data.noclassestoday) {
+      instname.current = result.data.instname;
+      instid.current = result.data.instid;
+      instdept.current = result.data.instdept;
+      coursedata.current = result.data.instinfo.rows;
+      setEditDept(instdept.current);
+      setEditName(instname.current);
+      setCoursesPresent(true);
+      setFetched(true);
+      return 1;
+    } else {
+      instname.current = result.data.instname;
+      instid.current = result.data.instid;
+      instdept.current = result.data.instdept;
+      coursedata.current = result.data.instinfo.rows;
+      today.current = result.data.today.rows;
+      setEditDept(instdept.current);
+      setEditName(instname.current);
+      setCoursesPresent(true);
+      setFetched(true);
+      setClassesToday(true);
+      return 1;
+    }
+  }
+
+  useEffect(() => {
+    infofetch();
+  }, [fetched, coursespresent, classestoday, edited]);
+
+  const togglepopup = async (e) => {
+    e.preventDefault();
+    setEditPopup(!editpopup);
+  }
+
+  var editpersonalinfo = async (e) => {
+    e.preventDefault();
+    if (editname === "" || editdept === "") {
+      alert("None of the fields should be left empty!");
+      return 0;
+    } else {
+      var wait = Axios.post("/editinstinfo", {
+        editname: editname,
+        editdept: editdept
+      }).then((response) => {
+        console.log(response);
+        if (response.data.message) {
+          alert("There seems to be a problem with our server. Please hang on while we fix it!");
         } else {
-            instname.current = result.data.instname;
-            instid.current = result.data.instid;
-            instdept.current = result.data.instdept;
-            coursedata.current = result.data.instinfo.rows;
-            today.current = result.data.today.rows;
-            setEditDept(instdept.current);
-            setEditName(instname.current);
-            setCoursesPresent(true);
-            setFetched(true);
-            setClassesToday(true);
-            return 1;
+          console.log("Ippudocchindhii");
+          window.location.reload();
+          // setEdited(!edited);
+          // setEditPopup(false);
         }
+        return response;
+      });
+      // if((await wait).data.message){
+      //     alert("There seems to be a problem with our server. Please hang on while we fix it!");
+      // } else{
+      //     console.log("Vacchindhi!");
+      //     setEdited(!edited);
+      //     setEditPopup(false);
+      // }
     }
+  }
 
-    useEffect(() => {
-        infofetch();
-    }, [fetched, coursespresent, classestoday, edited]);
+  const logout = () => {
+    // setLoginStatus(false);
+    Axios.get("http://localhost:8000/logout").then((response) => {
+      console.log(response);
+    });
+    navigate("/");
+  };
 
-    const togglepopup = async(e)=>{
-        e.preventDefault();
-        setEditPopup(!editpopup);
-    }
-
-    var editpersonalinfo = async(e)=>{
-        e.preventDefault();
-        if(editname==="" || editdept === ""){
-            alert("None of the fields should be left empty!");
-            return 0;
-        } else {
-            var wait = Axios.post("/editinstinfo",{
-                editname: editname,
-                editdept: editdept
-            }).then((response)=>{
-                console.log(response);
-                if(response.data.message){
-                    alert("There seems to be a problem with our server. Please hang on while we fix it!");
-                } else{
-                    console.log("Ippudocchindhii");
-                    window.location.reload();
-                    // setEdited(!edited);
-                    // setEditPopup(false);
-                }
-                return response;
-            });
-            // if((await wait).data.message){
-            //     alert("There seems to be a problem with our server. Please hang on while we fix it!");
-            // } else{
-            //     console.log("Vacchindhi!");
-            //     setEdited(!edited);
-            //     setEditPopup(false);
-            // }
-        }
-    }
-
-    const logout = () => {
-        // setLoginStatus(false);
-        Axios.get("http://localhost:8000/logout").then((response) => {
-            console.log(response);
-        });
-        navigate("/");
-    };
-    
   return (
-    <div className="nav-container">
-      <nav className="nav">
+    <div>
+      <nav>
         <Link to="/" className="site-title">
-          Site Name
+          We Care!
         </Link>
         <ul>
           <CustomLink to="/instructor">Home</CustomLink>
@@ -133,24 +133,43 @@ function NavBar() {
           <button onClick={logout} className='button3'>Logout</button>
         </ul>
       </nav>
-
-      
+      <div className='space-class'></div>
+      {/* <header></header> */}
     </div>
+    // <div className="nav-container backdrop-filter backdrop-blur-lg">
+    //   <nav className="sticky top-0 z-10 bg-white">
+    //   <div className="max-w-5xl mx-auto px-4">
+    // <div className="flex items-center justify-between h-16">
+    //   <span className="text-2xl text-gray-900 font-semibold">Logo</span>
+    //   <div className="flex space-x-4 text-gray-900">
+    //     <Link to="/" className="site-title">
+    //       We Care!
+    //     </Link>
+    //     <ul>
+    //       <CustomLink to="/instructor">Home</CustomLink>
+    //       <CustomLink to="/instructor/courses">Courses</CustomLink>
+    //       <button onClick={logout} className='button3'>Logout</button>
+    //     </ul>
+    //   </div></div></div>
+    //   </nav>
+
+
+    // </div>
   );
 
-  
+
 }
 
 export default NavBar;
 function CustomLink({ to, children, ...props }) {
-    const resolvedPath = useResolvedPath(to)
-    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
-  
-    return (
-      <li className={isActive ? "active" : ""}>
-        <Link to={to} {...props}>
-          {children}
-        </Link>
-      </li>
-    )
-  }
+  const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  )
+}
